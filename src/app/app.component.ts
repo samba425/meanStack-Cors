ae08c49762b86182d91a6b9cf42f015768d088c2
ALLA SAMBA SIVA RAO (asambasi) <asambasi@cisco.com>
2022-04-21 16:56:35 +0530

    wrapper added headers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from
+import { HttpClient, HttpHeaders } from
   '@angular/common/http';
 import { environment } from 'src/environments/environment';
 @Component({
@@ -15,50 +15,14 @@ export class AppComponent {
   constructor(private httpClient: HttpClient) { 
   }
 
-  getUser() {
-    let user = {
-      'username': 'first1l896fie@frizzmo-user.com',
-      'password': 'ironman01'
-    }
-    this.httpClient.post(`${this.url}/https://app-api-user-bff-centralus-dev-001.azurewebsites.net/api/auth/signUp`, user)
-      .subscribe((data) => {
-        this.success = data;
-        this.error = undefined;
-      }, error => {
-        this.error = (error && error['error']) ? error['error'] : error;
-        this.success = undefined
-      });
+  
+  test() {
+    let headers = new HttpHeaders()
+    headers = headers.set('Authorization', 'Basic dummy');
+    this.httpClient.request('POST', `${this.url}/{{ur external url}}`, { headers })
+    .subscribe(res => {
+      console.log('response',res)
+    })
   }
-  getStates() {
-    this.httpClient.get(`${this.url}/https://app-apsi-user-bff-centralus-dev-001.azurewebsites.net/api/states`)
-      .subscribe((data) => {
-        this.success = data;
-        this.error = undefined;
-      }, error => {
-        alert(1)
-        this.error = error;
-        this.success = undefined
-      });
-  }
-  signUp() {
-    let signUpPayload = {
-      'firstname': 'first1',
-      'lastname': 'last1',
-      'middlename': 'middle1',
-      'emailAddress': 'correct1@gmail.com',
-      'phoneNumber': '1',
-      'password': 'abcdefgh',
-      'deviceType': 'MOBILE',
-      'deviceId': 'sam1',
-      'isPrimary': true
-    }
-    this.httpClient.post(`${this.url}/https://app-api-user-bff-centralus-dev-001.azurewebsites.net/api/auth/signUp`, signUpPayload)
-      .subscribe((data) => {
-        this.success = data;
-        this.error = undefined;
-      }, error => {
-        this.error = (error && error['error']) ? error['error'] : error;
-        this.success = undefined
-      });
-  }
+
 }
